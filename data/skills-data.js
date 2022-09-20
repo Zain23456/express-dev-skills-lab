576570a455c360c66d24bd9f3b5e206ddc2ede28
Zain Ahmed <z.zain.ahmed@gmail.com>
2022-09-20 19:57:07 -0400

    add skills-data.js

---
+++ data/skills-data.js
@@ -0,0 +1,12 @@
+const skills = [
+  {name: 'HTML', done: true, _id: 1234},
+  {name: 'CSS', done: true, _id: 1235},
+  {name: 'Javascript', done: true, _id: 1236},
+  {name: 'Node.js', done: true, _id: 1237},
+  {name: 'Express', done: true, _id: 1238},
+  {name: 'MongoDB', done: true, _id: 1239},
+]
+
+export {
+  skills
+}
