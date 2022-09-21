a9d7fa728f56b4cb0a27baa40a63bf522a287daa
Zain Ahmed <z.zain.ahmed@gmail.com>
2022-09-20 21:13:36 -0400

    udpadte controller to complie from external database

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,9 +1,16 @@
-import { skills } from '../data/skills-data.js'
+import { Skill } from '../models/skills.js'
 
 
 function index(req, res) {
-  res.render('skills/index', {
-    skills: skills
+  Skill.find({})
+  .then(skills => {
+    res.render('skills/index', {
+      skills: skills
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/')
   })
 }
 
